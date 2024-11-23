2da4e1560ab3c6d891677826f4a6dcd73010c8e4
Burke Johnson <185158560+synth-mania@users.noreply.github.com>
2024-11-23 03:06:53 -0600

    add config folder to root

--- src/llm_config.py
+++ src/llm_config.py
@@ -1,5 +1,8 @@
 import json
 
+with open("config/parameter_defaults", 'r') as file:
+    PARAMETER_DEFAULT = json.load(file)
+
 def input_default(prompt, default):
     user_input = input(prompt)
     if user_input.strip() == '':
@@ -7,28 +10,19 @@ def input_default(prompt, default):
     else:
         return user_input
 
-
-def get_llm_config():
+def get_llm_config(preset_name = "default"):
     try:
-        with open("config.json", "r") as f:
+        with open(f"config/model_presets/{preset_name}.json", "r") as f:
             config = json.load(f)
         return config
     except FileNotFoundError:
         print("No config file found. Please provide the following parameters to connect to an OpenAI compatible API:")
 
-        api_key = input_default("API Key: ", "")
-        base_url = input_default("Base URL: ", "https://localhost:1234")
-        timeout = int(input_default("Timeout (seconds): ", 60))
-        max_tokens = int(input_default("Max tokens: ", 150))
-        
-        config = {
-            "api_key": api_key,
-            "base_url": base_url,
-            "timeout": timeout,
-            "max_tokens": max_tokens
-        }
-        
-        with open("config.json", "w") as f:
+        config = {}
+        for key, default in PARAMETER_DEFAULT.items():
+            value = input_default(f"{key}: ", default)
+            config[key] = value
+        with open("config/model_presets/{preset_name}.json", "w") as f:
             json.dump(config, f)
             
         return config
